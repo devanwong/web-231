703c0d5b614944e86f0970ec1e350753e19fe959
Devan Wong <devan.wong@yahoo.com>
2020-07-03 19:44:17 -0700

    submit week 6 assignment 6.2

--- Week 6/wong-assignment-6.2.js
+++ Week 6/wong-assignment-6.2.js
@@ -10,6 +10,7 @@ console.log(header.display('Devan', 'Wong', 'Assignment 6.2'))
 ;===========================================
 */
 
+//checking to see if the number is odd or even using modulus.
 try {
 
   var x = 5;
@@ -18,13 +19,13 @@ try {
 
 if (z == 1) throw "Number is odd";
 
-    console.log(z);
+    console.log(z + ' even number');
 
-} catch (err) {
+} catch (err) { //if statement true catch error runs.
 
     console.log("Catch clause: " + err);
 
-} finally {
+} finally { //end of try/catch
 
     console.log("Finally clause reached...");
 
